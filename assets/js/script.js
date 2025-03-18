5def88b7bad252a365b85a76a3df4d1b740e98f9
Rokesh Prakash <rokesh2897@gmail.com>
2025-03-18 01:31:30 -0400

    project sectino update

--- assets/js/script.js
+++ assets/js/script.js
@@ -174,38 +174,37 @@ function showSkills(skills) {
 }
 
 function showProjects(projects) {
-    // let viewButton = project.links.view ? `<a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>` : ``;
-    // let codeButton = project.links.code ? `<a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>` : ``;
-    // // let buttonsHTML = viewButton || codeButton ? `<div class="btns">${viewButton} ${codeButton}</div>` : "";
-
     let projectsContainer = document.querySelector("#work .box-container");
     let projectHTML = "";
    
-    projects.slice(0, 11).forEach(project => {
+    projects.slice(0, 13).forEach(project => {
+        let viewButton = project.links.view ? `<a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>` : "";
+        let codeButton = project.links.code ? `<a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>` : "";
+
+        // Only include buttons div if at least one button exists
+        let buttonsHTML = (viewButton || codeButton) ? `<div class="btns">${viewButton} ${codeButton}</div>` : "";
+
         projectHTML += `
         <div class="box tilt">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
-      <div class="content">
-        <div class="tag">
-        <h3>${project.name}</h3>
-        </div>
-        <div class="desc">
-          <p>${project.desc}</p>
-          <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+          <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+          <div class="content">
+            <div class="tag">
+              <h3>${project.name}</h3>
+            </div>
+            <div class="desc">
+              <p>${project.desc}</p>
+              ${buttonsHTML}  <!-- Only shows if buttons exist -->
+            </div>
           </div>
-        </div>
-      </div>
-    </div>`
+        </div>`;
     });
+
     projectsContainer.innerHTML = projectHTML;
 
     // <!-- tilt js effect starts -->
     VanillaTilt.init(document.querySelectorAll(".tilt"), {
         max: 15,
     });
-    // <!-- tilt js effect ends -->
 
     /* ===== SCROLL REVEAL ANIMATION ===== */
     const srtop = ScrollReveal({
@@ -217,7 +216,6 @@ function showProjects(projects) {
 
     /* SCROLL PROJECTS */
     srtop.reveal('.work .box', { interval: 200 });
-
 }
 
 fetchData().then(data => {
